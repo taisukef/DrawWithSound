63205b28376bcdebc7705cf533f0bf7c9b323f68
Taisuke Fukuno <fukuno@jig.jp>
2020-12-31 13:46:07 +0900

    Initial Commit

---
+++ Shared/DrawWithSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawWithSoundApp.swift
+//  Shared
+//
+//  Created by Taisuke Fukuno on 2020/12/31.
+//
+
+import SwiftUI
+
+@main
+struct DrawWithSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
