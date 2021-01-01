2d0cf064038c03b181305024774da9a7b345f2de
Taisuke Fukuno <fukuno@jig.jp>
2021-01-01 18:24:31 +0900

    fix screen refresh

--- Shared/DrawView.swift
+++ Shared/DrawView.swift
@@ -15,30 +15,17 @@ struct DrawPoints: Identifiable {
     var id = UUID()
 }
 
-struct DrawPathView: View {
-    var drawPointsArray: [DrawPoints] = []
-    init(drawPointsArray: [DrawPoints]) {
-        self.drawPointsArray = drawPointsArray
-    }
-    var body: some View {
-        ZStack {
-            ForEach(drawPointsArray) { data in
-                Path { path in
-                    path.addLines(data.points)
-                }
-                    .stroke(data.color, lineWidth: 10)
-            }
-        }
-    }
+class UserData: ObservableObject {
+    @Published var tmpDrawPoints: DrawPoints = DrawPoints(points: [], color: .red)
+    @Published var endedDrawPoints: [DrawPoints] = []
 }
 
 struct DrawView: View {
-    var audioGenerator = AudioGenerator()
-    @State var tmpDrawPoints: DrawPoints = DrawPoints(points: [], color: .red)
-    @State var endedDrawPoints: [DrawPoints] = []
-    @State var startPoint: CGPoint = CGPoint.zero
+    @ObservedObject var data: UserData = UserData()
     @State var selectedColor: Color = Color.red
-    
+    @State var startPoint: CGPoint = CGPoint.zero
+    var audioGenerator = AudioGenerator()
+
     init() {
         audioGenerator.setVolume(vol: 0.0)
         audioGenerator.start()
@@ -51,31 +38,36 @@ struct DrawView: View {
         VStack {
             Rectangle()
                 .foregroundColor(Color.white)
-                .frame(width: 400, height: 300, alignment: .center)
+                .frame(width: 300, height: 300, alignment: .center)
                 .overlay(
-                    DrawPathView(drawPointsArray: endedDrawPoints)
-                        .overlay(
+                    ZStack {
+                        ForEach(data.endedDrawPoints) { data in
                             Path { path in
-                                path.addLines(self.tmpDrawPoints.points)
+                                path.addLines(data.points)
                             }
-                                .stroke(self.tmpDrawPoints.color, lineWidth: 10)
-                        )
+                                .stroke(data.color, lineWidth: 10)
+                        }
+                        Path { path in
+                            path.addLines(data.tmpDrawPoints.points)
+                        }
+                            .stroke(data.tmpDrawPoints.color, lineWidth: 10)
+                    }
                 )
                 .gesture(
                     DragGesture()
                         .onChanged({ value in
                             if self.startPoint != value.startLocation {
-                                if self.tmpDrawPoints.points.count == 0 {
+                                if data.tmpDrawPoints.points.count == 0 {
                                     audioGenerator.setVolume(vol: 1.0)
                                 }
-                                self.tmpDrawPoints.points.append(value.location)
-                                self.tmpDrawPoints.color = self.selectedColor
+                                data.tmpDrawPoints.points.append(value.location)
+                                data.tmpDrawPoints.color = self.selectedColor
                             }
                         })
                         .onEnded({ value in
                             self.startPoint = value.startLocation
-                            self.endedDrawPoints.append(self.tmpDrawPoints)
-                            self.tmpDrawPoints = DrawPoints(points: [], color: self.selectedColor)
+                            data.endedDrawPoints.append(data.tmpDrawPoints)
+                            data.tmpDrawPoints = DrawPoints(points: [], color: self.selectedColor)
                             audioGenerator.setVolume(vol: 0.0)
                         })
                 )
@@ -84,8 +76,8 @@ struct DrawView: View {
                 Button(action: { self.selectedColor = Color.red }) { Text("赤") }
                 Button(action: { self.selectedColor = Color.white }) { Text("消しゴム") }
                 Button(action: {
-                    if self.endedDrawPoints.count > 0 {
-                        self.endedDrawPoints.removeLast()
+                    if data.endedDrawPoints.count > 0 {
+                        data.endedDrawPoints.removeLast()
                     }
                 }) { Text("やり直し") }
                 Spacer()
